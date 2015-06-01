cb277345a497d2d08f6157eba67cf01b66a7bcc3
Nick Lewis <nicklewis810@gmail.com>
2015-06-01 03:33:11 -0400

    added new registration update path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def after_update_path_for(resource)
-      user_path(resource)
+      users_path(resource)
     end
 end
